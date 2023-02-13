5ec9281763c5ffe261bd9cef6cd47ceaaa926fc9
koji <kojinb@gmail.com>
2023-02-13 11:35:22 -0500

    working en passant

--- src/Board.js
+++ src/Board.js
@@ -4,19 +4,20 @@ import Piece from './Piece';
 
 const Board = () => {
     const [game, setGame] = useState([
-        [Piece('rook', 'black'), Piece('knight', 'black'), Piece('bishop', 'black'), Piece('queen', 'black'), Piece('king','black'), Piece('bishop','black'), Piece('knight','black'), Piece('rook','black')],
+        [Piece('rook', 'black'), Piece('knight', 'black'), Piece('bishop', 'black'), Piece('queen', 'black'), Piece('king', 'black'), Piece('bishop', 'black'), Piece('knight', 'black'), Piece('rook', 'black')],
         Array(8).fill(Piece('pawn', 'black')),
         Array(8).fill(null),
         Array(8).fill(null),
         Array(8).fill(null),
         Array(8).fill(null),
         Array(8).fill(Piece('pawn', 'white')),
-        [Piece('rook', 'white'), Piece('knight', 'white'), Piece('bishop', 'white'), Piece('king','white'), Piece('queen', 'white'), Piece('bishop','white'), Piece('knight','white'), Piece('rook','white')],
+        [Piece('rook', 'white'), Piece('knight', 'white'), Piece('bishop', 'white'), Piece('king', 'white'), Piece('queen', 'white'), Piece('bishop', 'white'), Piece('knight', 'white'), Piece('rook', 'white')],
     ]);
 
     const [selected, setSelected] = useState({ x: null, y: null });
     const [invalidMove, setInvalidMove] = useState(false);
     const [whitesMove, setWhitesMove] = useState(true);
+    const [lastMove, setLastMove] = useState({x: null, y: null});
 
     const handleSquareClick = (x, y) => {
         if (selected.x === null) {
@@ -24,67 +25,107 @@ const Board = () => {
         } else {
             const gameCopy = JSON.parse(JSON.stringify(game));
             const piece = game[selected.y][selected.x];
-            let isValidMove = false;
+            let isValidMove = {validMove: false, enPassant: false};
             switch (piece.name) {
                 case 'pawn':
-                    isValidMove = isValidPawnMove(game, selected, x, y);
+                    isValidMove = isValidPawnMove(game, selected, x, y, lastMove);
                     break;
                 case 'rook':
                     isValidMove = isValidRookMove(game, selected, x, y);
                     break;
                 // cases for other pieces
             }
-            if (isValidMove) {
+            if (isValidMove.validMove) {
+                if (isValidMove.enPassant) {
+                    gameCopy[lastMove.y][lastMove.x] = null;
+                }
                 gameCopy[selected.y][selected.x] = null;
                 gameCopy[y][x] = game[selected.y][selected.x];
                 setGame(gameCopy);
                 setInvalidMove(false);
                 setWhitesMove(!whitesMove);
+                setLastMove({x, y});
             } else {
                 console.log('Invalid move');
                 setInvalidMove(true);
             }
-            setSelected({ x: null, y: null});
+            setSelected({ x: null, y: null });
         }
     };
 
-    const isValidPawnMove = (game, selected, x, y, color) => {
+    const isValidPawnMove = (game, selected, x, y, previous_move) => {
         // Destructure the x and y values of the selected square from the `selected` object
         const { x: selectedX, y: selectedY } = selected;
+        const { x: previousX, y: previousY } = previous_move;
         const piece = game[selectedY][selectedX];
 
         // Check if the selected pawn is white
         if (piece.color === 'white' && whitesMove) {
             // Check if the pawn is moving forward one square and there is no piece in that square
             if (y === selectedY - 1 && x === selectedX && !game[y][x]) {
-                return true;
+                return {
+                    validMove: true, 
+                    enPassant: false
+                };
             }
             // Check if the pawn is capturing a black piece
             else if (y === selectedY - 1 && Math.abs(x - selectedX) === 1 && game[y][x] && game[y][x].color === 'black') {
-                return true;
+                return {
+                    validMove: true,
+                    enPassant: false
+                };
             }
             // Check for white pawn's initial two-square move
             else if (selectedY === 6 && y === 4 && x === selectedX && !game[5][x] && !game[4][x]) {
-                return true;
+                return {
+                    validMove: true,
+                    enPassant: false
+                };
+            }
+            // Check for en passant
+            else if (selectedY === previousY && x === previousX && Math.abs(selectedX - x) === 1 && game[previousY][previousX].color === 'black' && game[previousY][previousX].name === 'pawn'){
+                return {
+                    validMove: true, 
+                    enPassant: true
+                };
             }
         }
         // Check if the selected pawn is black
         else {
             // Check if the pawn is moving forward one square and there is no piece in that square
             if (y === selectedY + 1 && x === selectedX && !game[y][x]) {
-                return true;
+                return {
+                    validMove: true,
+                    enPassant: false
+                };
             }
             // Check if the pawn is capturing a white piece
             else if (y === selectedY + 1 && Math.abs(x - selectedX) === 1 && game[y][x] && game[y][x].color === 'white') {
-                return true;
+                return {
+                    validMove: true,
+                    enPassant: false
+                };
             }
             // Check for black pawn's initial two-square move
             else if (selectedY === 1 && y === 3 && x === selectedX && !game[2][x] && !game[3][x]) {
-                return true;
+                return {
+                    validMove: true,
+                    enPassant: false
+                };
+            }
+            // check for black's en passant capture
+            else if (selectedY === previousY && x === previousX && Math.abs(selectedX - x) === 1 && game[previousY][previousX].color === 'white' && game[previousY][previousX].name === 'pawn'){
+                return {
+                    validMove: true, 
+                    enPassant: true
+                };
             }
         }
         // Return false if the move is not valid
-        return false;
+        return {
+            validMove: false,
+            enPassant: false
+        };
     };
 
     const isValidRookMove = (game, selected, x, y) => {
@@ -122,6 +163,7 @@ const Board = () => {
                 {squares}
             </div>
             <div>selected x: {selected.x}, selected y: {selected.y}</div>
+            <div>last move: {lastMove.x}, {lastMove.y}</div>
             {invalidMove && <div>Invalid Move!</div>}
         </>
     );
